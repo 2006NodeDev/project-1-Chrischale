0ed7e881b0c374f912737c069d959bfba4a1a1eb
Chrischale Panditharathne <cpanda27@bu.edu>
2020-07-20 13:02:47 -0400

    changing baseURL

--- FrontEnd/src/Remote/index.ts
+++ FrontEnd/src/Remote/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const proj1Client = axios.create({
-    baseURL:'http://localhost:2020',
+    //baseURL:'http://localhost:2020',
+    baseURL:'http://35.190.10.193:80',
     headers:{
         'Content-Type':'application/json'
     },
